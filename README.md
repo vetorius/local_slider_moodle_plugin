3caac7412a147e454841ddf688c3d599ab9c43aa
Víctor Manuel Sánchez <vetorius@gmail.com>
2021-04-27 08:52:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Local slider
+
+## Funcionalidad
+- Almacenar los sliders para los cursos NCA
+- Permitir que el json de un slider sea accesible desde el propio moodle
+- Permitir actualizar los sliders desde una app remota via servicio web
---
+++ version.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Version details
+ *
+ * @package    local_slider
+ * @author     Víctor M. Sanchez
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @var stdClass $plugin
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_slider';
+$plugin->version   = 2021042600;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2020110300;        // Requires this Moodle version
