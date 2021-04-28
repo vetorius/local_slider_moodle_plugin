2c25a2b75bd8269899ca8d6e26cc32a0d70c4335
Víctor Manuel Sánchez <vetorius@gmail.com>
2021-04-28 11:01:35 +0200

    update header comments in files

--- lang/en/local_slider.php
+++ lang/en/local_slider.php
@@ -18,40 +18,11 @@
  */
 
 /**
- * getslider service returns JSON 
- * optional paramenter slidername
+ * local_slider english language definitions
  *
  * @package    local_slider
  * @author     Víctor M. Sanchez
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require(__DIR__. '/../../config.php');
-
-global $DB;
-
-// get slidername parameter
-$slidername = optional_param('slidername', '', PARAM_RAW);
-
-$PAGE->set_url(new moodle_url('/local/slider/getslider.php'));
-
-
-// only logged users can access
-require_login();
-
-// get slider data from database
-if ($sliderdata = $DB->get_record('local_slider', array('name'=>$slidername), 'data')) {
-    $jsondata = $sliderdata->data;
-} else {
-    $jsondata = '{ }';
-}
-
-
-// send mime type and encoding
-@header('Content-Type: application/json; charset=utf-8');
-//@header('Content-Type: text/plain; charset=utf-8');
-// we do not want html markup
-@ini_set('html_errors', 'off');
-
-// send json data
-echo $jsondata;
+$string['pluginname'] = 'Slider';
